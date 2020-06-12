5ca672ff06ee7ec9e8bf4017de5750ac9f0fd551
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-11 22:56:49 -0300

    ⚡ Threads and Async exercises resolved

---
+++ Exercicio01-2Threads0a100/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercicio01_2Threads0a100.Apresentacao
+{
+    public class Program
+    {
+        static readonly int fifty = 50;
+        static readonly int oneHundred = 100;
+        static void Main(string[] args)
+        {
+            Thread threadZeroToFifty = new Thread(PrintZeroToFifty);
+            Thread threadFiftyOneToOneHundred = new Thread(PrintFiftyOneToOneHundred);
+
+            threadZeroToFifty.Start();
+            threadFiftyOneToOneHundred.Start();
+
+            Console.ReadKey();
+        }
+
+        static void PrintZeroToFifty()
+        {
+            Console.WriteLine("Iniciando 1ª Thread:");
+            Thread.Sleep(1000);
+
+            for (int i = 0; i < fifty; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("1ª Thread finalizada");
+        }
+
+        static void PrintFiftyOneToOneHundred()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Iniciando 2ª Thread:");
+            Thread.Sleep(2000);            
+
+            for (int i = fifty; i <= oneHundred; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("2ª Thread finalizada");
+        }
+    }
+}
---
+++ Exercicio02_MetodoAssincrono.Apresentacao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercicio02_MetodoAssincrono.Apresentacao
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(BuscaAlunoAsync().Result);
+            Console.ReadKey();
+        }
+
+        private static async Task<string> BuscaAlunoAsync()
+        {
+            var aluno = string.Empty;
+            await Task.Run(() =>
+            {
+                Loading();                
+                aluno = "Aluno: Samir Turquetto Melo \nR.A.: 290";
+            });
+
+            return aluno;
+        }
+
+        private static void Loading()
+        {
+            Console.WriteLine("Buscando aluno...");
+            for (int i = 10; i > 0; i--)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Aluno encontrado!");
+        }
+    }
+}
